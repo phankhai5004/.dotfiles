3a09eb4a704649e93b1b3fd5a1e738fe66b4d9ee
KhaiTheDev <phankhai5004@gmail.com>
2024-04-02 21:35:05 +0700

    new commit 04/02

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -4,17 +4,17 @@ local options = {
     cpp = { "clang-format" },
     css = { "prettierd" },
     html = { "prettierd" },
-    -- javascript = { "prettierd" },
-    -- typescript = { "prettierd" },
-    -- javascriptreact = { "prettierd" },
-    -- typescriptreact = { "prettierd" },
+    javascript = { "prettierd" },
+    typescript = { "prettierd" },
+    javascriptreact = { "prettierd" },
+    typescriptreact = { "prettierd" },
   },
 
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+  -- format_on_save = {
+  --   -- These options will be passed to conform.format()
+  --   timeout_ms = 500,
+  --   lsp_fallback = true,
+  -- },
 }
 
 require("conform").setup(options)
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -15,6 +15,14 @@ return {
     cmd = "GitBlameToggle",
   },
 
+  {
+    "sindrets/diffview.nvim",
+    cmd = { "DiffviewOpen", "DiffviewToggleFiles" },
+    config = function()
+      require("diffview").setup()
+    end,
+  },
+
   --USER INTERFACE
   {
     "RRethy/vim-illuminate",
@@ -57,6 +65,35 @@ return {
       require("nvchad.configs.lspconfig").defaults()
       require "configs.lspconfig"
     end,
+  },
+
+  {
+    "numToStr/Comment.nvim",
+    dependencies = "JoosepAlviste/nvim-ts-context-commentstring",
+    config = function()
+      require("Comment").setup {
+        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+      }
+    end,
+  },
+
+  {
+    "folke/trouble.nvim",
+    cmd = { "Trouble", "TodoTrouble" },
+    dependencies = {
+      {
+        "folke/todo-comments.nvim",
+        opts = {},
+      },
+    },
+    config = function()
+      require("trouble").setup()
+    end,
+  },
+
+  {
+    "mbbill/undotree",
+    cmd = { "UndotreeToggle" },
   },
 
   {
