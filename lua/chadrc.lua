2ed4a49376018b2fd2600eed43f4908a324a4e43
KhaiTheDev <phankhai5004@gmail.com>
2024-04-02 16:05:29 +0700

    04/02 config

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -25,6 +25,32 @@ M.ui = {
     modules = nil,
   },
 
+  nvdash = {
+    load_on_startup = true,
+
+    header = {
+      [[                                                                       ]],
+      [[                                                                     ]],
+      [[       ████ ██████           █████      ██                     ]],
+      [[      ███████████             █████                             ]],
+      [[      █████████ ███████████████████ ███   ███████████   ]],
+      [[     █████████  ███    █████████████ █████ ██████████████   ]],
+      [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+      [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+      [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+      [[                                                                       ]],
+    },
+
+    buttons = {
+      { "  Find File", "Spc f f", "Telescope find_files" },
+      { "󰈚  Recent Files", "Spc f o", "Telescope oldfiles" },
+      { "󰈭  Find Word", "Spc f w", "Telescope live_grep" },
+      { "  Bookmarks", "Spc m a", "Telescope marks" },
+      { "  Themes", "Spc t h", "Telescope themes" },
+      { "  Mappings", "Spc c h", "NvCheatsheet" },
+    },
+  },
+
   term = {
     hl = "Normal:term,WinSeparator:WinSeparator",
     sizes = { sp = 0.3, vsp = 0.2 },
--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -9,7 +9,9 @@ local servers = { "clangd", "lua_ls", "html", "cssls", "jsonls", "dockerls", "em
 local handlers = {
   ["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" }),
   ["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" }),
-  ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, { border = "rounded" }),
+  vim.diagnostic.config {
+    float = { border = "rounded" },
+  },
 }
 
 -- lsps with default config
