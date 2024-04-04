2edf69572cd2758c8fc98eca0a84f180d355a205
KhaiTheDev <phankhai5004@gmail.com>
2024-04-04 21:32:05 +0700

    day 04/04

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -10,11 +10,11 @@ local options = {
     typescriptreact = { "prettierd" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 10000,
+    lsp_fallback = true,
+  },
 }
 
 require("conform").setup(options)
